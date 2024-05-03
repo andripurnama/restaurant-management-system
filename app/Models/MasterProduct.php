169ea86bc06fd77a0973013ee086573a31af00f5
andripurnama <andri.purnama995@hotmail.com>
2024-05-03 18:14:34 +0700

    feat: added MasterProduct model

--- app/Models/MasterProduct.php
+++ app/Models/MasterProduct.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MasterProduct extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $fillable = ['ProductId','ProductName','ProductCategory','SellingPrice'];
+    protected $primaryKey = 'ProductId';
+    public function ProductVariant()
+    {
+        return $this->hasMany(ProductVariant::class,'ProductId','ProductId');
+    }
 }
