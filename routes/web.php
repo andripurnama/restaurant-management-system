6fe5db1497e7f4238576cc88c9b0bb39ba2b91cf
andripurnama <andri.purnama995@hotmail.com>
2024-05-04 00:18:10 +0700

    feat: added product route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+Route::resource('product',ProductController::class);
