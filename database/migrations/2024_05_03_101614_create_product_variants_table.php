2dbb943d3f0c1ce345652a7c6df416fc4eb5b064
andripurnama <andri.purnama995@hotmail.com>
2024-05-03 18:52:42 +0700

    feat: added fields on product_variants migration file

--- database/migrations/2024_05_03_101614_create_product_variants_table.php
+++ database/migrations/2024_05_03_101614_create_product_variants_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\View\Components\Task;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +13,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('product_variants', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('VariantId');
+            $table->unsignedBigInteger('ProductId');
+            $table->string('VariantName');
+            $table->string('sku')->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
