fed4cce641f4416285f1967da12f5183b9db9f02
andripurnama <andri.purnama995@hotmail.com>
2024-05-03 18:14:58 +0700

    feat: added ProductVariant model

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductVariant extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    protected $fillable = ['VariantId','ProductId','VariantName','sku'];
+
+    public function product()
+    {
+        return $this->belongsTo(MasterProduct::class,'ProductId','ProductId');
+    }
 }
